5695a4669a7b687a6b1890aebb458ca9d5191c3d
thora_9 <tejasvi_9@hotmail.com>
2018-02-12 16:43:06 -0500

    Small additions

--- my_rain_tester.R
+++ my_rain_tester.R
@@ -7,25 +7,29 @@ freq_rainfall=rpois(days,0.3)
 
 n=10
 alpha=10
-lambda=0.3
+lambda=0.9
 
 myrain <- function(n,alpha,lambda){
   days=365*n
-  freq_rainfall=rpois(days,lambda)
+  freq_rainfall=round(rexp(days,lambda))
   freq_rainfall[1]=0
   out_rain=alpha
   i=2
   for (i in 1:days){
     tau1=freq_rainfall[i]
     if (tau1==0){
-      cur_rain=rexp(1,1/10)
+      cur_rain=rexp(1,1/alpha)
     } else {
       no_rain_days=rep(0,tau1)
-      next_rain_day=rexp(1,1/10)
+      next_rain_day=rexp(1,1/alpha)
       cur_rain=c(no_rain_days,next_rain_day)
-      i=i+tau1+1
+      #i=i+tau1+1
       }
     out_rain=c(out_rain,cur_rain)
   }
-  return(out_rain)
+  sum(out_rain[1:365])
+  
+  return(out_rain[1:days])
 }
+
+
